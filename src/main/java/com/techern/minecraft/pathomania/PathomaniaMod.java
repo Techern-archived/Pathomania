838e35611474c1633b604fa989f8f79d214a18cc
Cruz Julian Bishop <cruzjbishop@gmail.com>
2015-11-10 11:00:46 +1000

    Get everything to a clean slate

---
+++ src/main/java/com/techern/minecraft/pathomania/PathomaniaMod.java
@@ -0,0 +1,27 @@
+package com.techern.minecraft.pathomania;
+
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * The {@link Mod} descriptor for Pathomania
+ *
+ * @since 0.0.1
+ */
+@Mod(acceptableRemoteVersions = PathomaniaMod.MOD_VERSION, modid = PathomaniaMod.MOD_ID, version = PathomaniaMod.MOD_VERSION)
+public class PathomaniaMod {
+
+    /**
+     * The name of the Pathomania {@link Mod}
+     *
+     * @since 0.0.1
+     */
+    public static final String MOD_ID = "pathomania";
+
+    /**
+     * The version of the Pathomania {@link Mod}
+     *
+     * @since 0.0.1
+     */
+    public static final String MOD_VERSION = "0.0.1-SNAPSHOT";
+
+}
